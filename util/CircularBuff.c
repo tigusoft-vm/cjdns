001987be2ff5f38fb835a891d211f60a676fa2f5
rob <rob@mail.l>
2015-11-26 15:20:15 +0000

    send_buffer in push/pop

--- util/CircularBuff.c
+++ util/CircularBuff.c
@@ -1,146 +1,193 @@
-// #include "util/CircularBuff.h"
-//
-// #include <assert.h>
-// #include <stdlib.h>
-// //private functions
-//
-// /**
-//  * Call free for single uv_buf_t
-//  */
-// static void free_buff(uv_buf_t *buff) {
-// 	assert(buff != NULL);
-// 	free(buff->base);
-// 	buff->base = NULL;
-// 	buff->len = 0;
-// }
-//
-// /**
-//  * Move current_element pointer to next element
-//  */
-// static void move_internal_pointer(uv_buff_circular * const circular_buff) {
-// 	assert(circular_buff != NULL);
-// 	// Move 'current_element' pointer
-// 	// check if current_element == last element
-// 	if (circular_buff->current_element == &circular_buff->buffs[circular_buff->max_size -1]) {
-// 		circular_buff->current_element = &circular_buff->buffs[0];
-// 	}
-// 	else {
-// 		circular_buff->current_element++;
-// 	}
-// }
-//
-// // public functions
-//
-// /**
-//  * @param circular_buff Must be allocated in caller.
-//  * @param nbufs Number of buffers
-//  */
-// void CircularBuffInit(uv_buff_circular *circular_buff, size_t nbufs) {
-// 	assert(circular_buff != NULL);
-// 	circular_buff->buffs = (uv_buf_t *)malloc(sizeof(uv_buf_t) * nbufs);
-// 	for (size_t i = 0; i < nbufs; ++i) {
-// 		circular_buff->buffs[i].base = NULL;
-// 		circular_buff->buffs[i].len = 0;
-// 	}
-// 	circular_buff->max_size = nbufs;
-// 	circular_buff->current_element = &circular_buff->buffs[nbufs -1];
-// 	circular_buff->size = 0;
-// }
-//
-// /**
-//  * Move data from @param buff to @param circular_buff
-//  * @param circular_buff Initialized by caller (buff_circular_init).
-//  * @param buff Initialized by caller. Clean in this function (.base = NULL, .len=0).
-//  * @return 0 if success
-//  */
-// int CircularBuffPush(uv_buff_circular * const circular_buff, uv_buf_t * const buff) {
-// 	if (circular_buff == NULL) {
-// 		return 1;
-// 	}
-// 	if (buff == NULL) {
-// 		return 2;
-// 	}
-//
-// 	assert(circular_buff->size <= circular_buff->max_size);
-// 	if (circular_buff->size == circular_buff->max_size) { // buffer if full
-// 		return 3;
-// 	}
-//
-// 	// move 'current_element' pointer to next slot
-// 	move_internal_pointer(circular_buff);
-//
-// 	// move element
-// 	circular_buff->current_element->len = buff->len;
-// 	buff->len = 0;
-// 	circular_buff->current_element->base = buff->base;
-// 	buff->base = NULL;
-//
-// 	// increment size
-// 	if (circular_buff->size < circular_buff->max_size) {
-// 		circular_buff->size++;
-// 	}
-// 	return 0;
-// }
-//
-// /**
-//  * Move data from @param circular_buff to @param buff
-//  * @param circular_buff Initialized by caller (buff_circular_init).
-//  * @param buff Out pointer. Must be empty (.base = NULL, .len=0).
-//  * @return 0 if success
-//  */
-// int CircularBuffPop(uv_buff_circular *circular_buff, uv_buf_t * const buff) {
-// 	if (circular_buff == NULL) {
-// 		return 1;
-// 	}
-// 	if (buff == NULL) {
-// 		return 2;
-// 	}
-//
-// 	assert(buff->base == NULL);
-// 	assert(buff->len == 0);
-//
-// 	size_t pop_element_index = circular_buff->max_size;
-//
-// 	uv_buf_t *pop_ptr = circular_buff->current_element;
-// 	for (size_t i = 0; i < circular_buff->size - 1; ++i) {
-// 		if (pop_ptr == &circular_buff->buffs[0]) {
-// 			pop_ptr += circular_buff->max_size - 1; // last element in array
-// 		}
-// 		else {
-// 			pop_ptr--;
-// 		}
-// 	}
-//
-// 	assert(pop_element_index <= circular_buff->max_size);
-//
-// 	// move buffer
-// 	buff->base = pop_ptr->base;
-// 	pop_ptr->base = NULL;
-// 	buff->len = pop_ptr->len;
-// 	pop_ptr->len = 0;
-//
-// 	circular_buff->size--;
-//
-// 	return 0;
-// }
-//
-// /**
-//  * Call free() for evry element.
-//  * Deallocate internal array.
-//  * Instance of struct 'circular_buff' will be not deallocate.
-//  */
-// void CircularBuffDeinit(uv_buff_circular * const circular_buff) {
-// 	if (circular_buff == NULL) {
-// 		return;
-// 	}
-//
-// 	for (size_t i = 0; i < circular_buff->max_size; ++i) {
-// 		if (circular_buff->buffs[i].base != NULL)
-// 			free_buff(&circular_buff->buffs[i]);
-// 	}
-// 	free(circular_buff->buffs);
-// 	circular_buff->current_element = NULL;
-// 	circular_buff->buffs = NULL;
-// 	circular_buff->max_size = 0;
-// 	circular_buff->size = 0;
-// }
+/* vim: set expandtab ts=4 sw=4: */
+/*
+ * You may redistribute this program and/or modify it under the terms of
+ * the GNU General Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include "util/CircularBuff.h"
+
+#include <assert.h>
+#include <stdlib.h>
+
+static void free_buff(send_buffer *buff)
+{
+    assert(buff != NULL);
+    free(buff->buffer->base);
+    buff->buffer->base = NULL;
+    buff->buffer->len = 0;
+}
+
+/**
+ * Move current_element pointer to next element
+ */
+static void move_internal_pointer(uv_buff_circular * const circular_buff)
+{
+    assert(circular_buff != NULL);
+    // Move 'current_element' pointer
+    // check if current_element == last element
+    if (circular_buff->current_element == &circular_buff->buffs[circular_buff->max_size -1])
+    {
+        circular_buff->current_element = &circular_buff->buffs[0];
+    }
+    else
+    {
+        circular_buff->current_element++;
+    }
+}
+
+// public functions
+
+/**
+ * @param circular_buff Must be allocated in caller.
+ * @param nbufs Number of buffers
+ */
+void CircularBuffInit(uv_buff_circular *circular_buff, size_t nbufs)
+{
+    assert(circular_buff != NULL);
+    circular_buff->buffs = (send_buffer *)malloc(sizeof(send_buffer) * nbufs);
+    for (size_t i = 0; i < nbufs; ++i)
+    {
+        circular_buff->buffs[i].buffer->base = NULL;
+        circular_buff->buffs[i].buffer->len = 0;
+    }
+    circular_buff->max_size = nbufs;
+    circular_buff->current_element = &circular_buff->buffs[nbufs -1];
+    circular_buff->size = 0;
+}
+
+/**
+ * Move data from @param buff to @param circular_buff
+ * @param circular_buff Initialized by caller (buff_circular_init).
+ * @param buff Initialized by caller. Clean in this function (.base = NULL, .len=0).
+ * @return 0 if success
+ */
+int CircularBuffPush(uv_buff_circular * const circular_buff, send_buffer * const buff)
+{
+    if (circular_buff == NULL)
+    {
+        return 1;
+    }
+    if (buff == NULL)
+    {
+        return 2;
+    }
+
+    assert(circular_buff->size <= circular_buff->max_size);
+    if (circular_buff->size == circular_buff->max_size)  // buffer if full
+    {
+        return 3;
+    }
+
+    // move 'current_element' pointer to next slot
+    move_internal_pointer(circular_buff);
+
+    // move element
+    circular_buff->current_element->buffer->len = buff->buffer->len;
+    buff->buffer->len = 0;
+    circular_buff->current_element->buffer->base = buff->buffer->base;
+    buff->buffer->base = NULL;
+    circular_buff->current_element->req = buff->req;
+    buff->req = NULL;
+    circular_buff->current_element->handle = buff->handle;
+    buff->handle = NULL;
+    circular_buff->current_element->addr = buff->addr;
+    buff->addr = NULL;
+    circular_buff->current_element->send_cb = buff->send_cb;
+    buff->send_cb = NULL;
+
+    // increment size
+    if (circular_buff->size < circular_buff->max_size)
+    {
+        circular_buff->size++;
+    }
+    return 0;
+}
+
+/**
+ * Move data from @param circular_buff to @param buff
+ * @param circular_buff Initialized by caller (buff_circular_init).
+ * @param buff Out pointer. Must be empty (.base = NULL, .len=0).
+ * @return 0 if success
+ */
+int CircularBuffPop(uv_buff_circular *circular_buff, send_buffer * const buff)
+{
+    if (circular_buff == NULL)
+    {
+        return 1;
+    }
+    if (buff == NULL)
+    {
+        return 2;
+    }
+
+    assert(buff->buffer->base == NULL);
+    assert(buff->buffer->len == 0);
+
+    size_t pop_element_index = circular_buff->max_size;
+
+    send_buffer *pop_ptr = circular_buff->current_element;
+    for (size_t i = 0; i < circular_buff->size - 1; ++i)
+    {
+        if (pop_ptr == &circular_buff->buffs[0])
+        {
+            pop_ptr += circular_buff->max_size - 1; // last element in array
+        }
+        else {
+            pop_ptr--;
+        }
+    }
+
+    assert(pop_element_index <= circular_buff->max_size);
+
+    // move buffer
+    buff->buffer->base = pop_ptr->buffer->base;
+    pop_ptr->buffer->base = NULL;
+    buff->buffer->len = pop_ptr->buffer->len;
+    pop_ptr->buffer->len = 0;
+    buff->req = pop_ptr->req;
+    pop_ptr->req = NULL;
+    buff->handle = pop_ptr->handle;
+    pop_ptr->handle = NULL;
+    buff->addr = pop_ptr->addr;
+    pop_ptr->addr = NULL;
+    buff->send_cb = pop_ptr->send_cb;
+    pop_ptr->send_cb = NULL;
+
+    circular_buff->size--;
+
+    return 0;
+}
+
+/**
+ * Call free() for evry element.
+ * Deallocate internal array.
+ * Instance of struct 'circular_buff' will be not deallocate.
+ */
+void CircularBuffDeinit(uv_buff_circular * const circular_buff)
+{
+    if (circular_buff == NULL)
+    {
+        return;
+    }
+
+    for (size_t i = 0; i < circular_buff->max_size; ++i)
+    {
+        if (circular_buff->buffs[i].buffer->base != NULL)
+        {
+            free_buff(&circular_buff->buffs[i]);
+        }
+    }
+    free(circular_buff->buffs);
+    circular_buff->current_element = NULL;
+    circular_buff->buffs = NULL;
+    circular_buff->max_size = 0;
+    circular_buff->size = 0;
+}
--- util/CircularBuff.h
+++ util/CircularBuff.h
@@ -15,11 +15,10 @@
 #ifndef CircularBuff_H
 #define CircularBuff_H
 
-//#include "util/Linker.h"
-//Linker_require("util/CircularBuff.c")
-
-#include <pthread.h>
 #include <uv.h>
+
+#include "util/Linker.h"
+Linker_require("util/CircularBuff.c")
 
 #include <assert.h>
 #include <stdlib.h>
@@ -45,169 +44,12 @@ typedef struct uv_buff_circular
     send_buffer *current_element; // last element
 } uv_buff_circular;
 
-/*void CircularBuffInit(uv_buff_circular *circular_buff, size_t nbufs);
-int CircularBuffPush(uv_buff_circular * const circular_buff, uv_buf_t * const buff);
-int CircularBuffPop(uv_buff_circular *circular_buff, uv_buf_t * const buff);
+void CircularBuffInit(uv_buff_circular *circular_buff, size_t nbufs);
+int CircularBuffPush(uv_buff_circular * const circular_buff, send_buffer * const buff);
+int CircularBuffPop(uv_buff_circular *circular_buff, send_buffer * const buff);
 void CircularBuffDeinit(uv_buff_circular * const circular_buff);
-*/
 
-static void free_buff(send_buffer *buff)
-{
-    assert(buff != NULL);
-    free(buff->buffer->base);
-    buff->buffer->base = NULL;
-    buff->buffer->len = 0;
-}
 
-/**
- * Move current_element pointer to next element
- */
-static void move_internal_pointer(uv_buff_circular * const circular_buff)
-{
-    assert(circular_buff != NULL);
-    // Move 'current_element' pointer
-    // check if current_element == last element
-    if (circular_buff->current_element == &circular_buff->buffs[circular_buff->max_size -1])
-    {
-        circular_buff->current_element = &circular_buff->buffs[0];
-    }
-    else
-    {
-        circular_buff->current_element++;
-    }
-}
 
-// public functions
-
-/**
- * @param circular_buff Must be allocated in caller.
- * @param nbufs Number of buffers
- */
-void CircularBuffInit(uv_buff_circular *circular_buff, size_t nbufs)
-{
-    assert(circular_buff != NULL);
-    circular_buff->buffs = (send_buffer *)malloc(sizeof(send_buffer) * nbufs);
-    for (size_t i = 0; i < nbufs; ++i)
-    {
-        circular_buff->buffs[i].buffer->base = NULL;
-        circular_buff->buffs[i].buffer->len = 0;
-    }
-    circular_buff->max_size = nbufs;
-    circular_buff->current_element = &circular_buff->buffs[nbufs -1];
-    circular_buff->size = 0;
-}
-
-/**
- * Move data from @param buff to @param circular_buff
- * @param circular_buff Initialized by caller (buff_circular_init).
- * @param buff Initialized by caller. Clean in this function (.base = NULL, .len=0).
- * @return 0 if success
- */
-int CircularBuffPush(uv_buff_circular * const circular_buff, uv_buf_t * const buff)
-{
-    if (circular_buff == NULL)
-    {
-        return 1;
-    }
-    if (buff == NULL)
-    {
-        return 2;
-    }
-
-    assert(circular_buff->size <= circular_buff->max_size);
-    if (circular_buff->size == circular_buff->max_size)  // buffer if full
-    {
-        return 3;
-    }
-
-    // move 'current_element' pointer to next slot
-    move_internal_pointer(circular_buff);
-
-    // move element
-    circular_buff->current_element->buffer->len = buff->len;
-    buff->len = 0;
-    circular_buff->current_element->buffer->base = buff->base;
-    buff->base = NULL;
-
-    // increment size
-    if (circular_buff->size < circular_buff->max_size)
-    {
-        circular_buff->size++;
-    }
-    return 0;
-}
-
-/**
- * Move data from @param circular_buff to @param buff
- * @param circular_buff Initialized by caller (buff_circular_init).
- * @param buff Out pointer. Must be empty (.base = NULL, .len=0).
- * @return 0 if success
- */
-int CircularBuffPop(uv_buff_circular *circular_buff, uv_buf_t * const buff)
-{
-    if (circular_buff == NULL)
-    {
-        return 1;
-    }
-    if (buff == NULL)
-    {
-        return 2;
-    }
-
-    assert(buff->base == NULL);
-    assert(buff->len == 0);
-
-    size_t pop_element_index = circular_buff->max_size;
-
-    send_buffer *pop_ptr = circular_buff->current_element;
-    for (size_t i = 0; i < circular_buff->size - 1; ++i)
-    {
-        if (pop_ptr == &circular_buff->buffs[0])
-        {
-            pop_ptr += circular_buff->max_size - 1; // last element in array
-        }
-        else {
-            pop_ptr--;
-        }
-    }
-
-    assert(pop_element_index <= circular_buff->max_size);
-
-    // move buffer
-    buff->base = pop_ptr->buffer->base;
-    pop_ptr->buffer->base = NULL;
-    buff->len = pop_ptr->buffer->len;
-    pop_ptr->buffer->len = 0;
-
-    circular_buff->size--;
-
-    return 0;
-}
-
-/**
- * Call free() for evry element.
- * Deallocate internal array.
- * Instance of struct 'circular_buff' will be not deallocate.
- */
-void CircularBuffDeinit(uv_buff_circular * const circular_buff)
-{
-    if (circular_buff == NULL)
-    {
-        return;
-    }
-
-    for (size_t i = 0; i < circular_buff->max_size; ++i)
-    {
-        if (circular_buff->buffs[i].buffer->base != NULL)
-        {
-            free_buff(&circular_buff->buffs[i]);
-        }
-    }
-    free(circular_buff->buffs);
-    circular_buff->current_element = NULL;
-    circular_buff->buffs = NULL;
-    circular_buff->max_size = 0;
-    circular_buff->size = 0;
-}
 
 #endif
