3936da5fd8a26d0985d196098b782f929b8f2085
win <win@example.com>
2015-12-11 17:01:46 +0100

    set second dns

--- interface/tuntap/windows/main_dns.c
+++ interface/tuntap/windows/main_dns.c
@@ -7,7 +7,7 @@
 #define NAME_SIZE 256
 #define REG_KEY_PATH_PREFIX "SYSTEM\\ControlSet001\\services\\TCPIP6\\Parameters\\Interfaces\\"
 
-int set_dns_for_tun(const char *dns_address)
+int set_dns_for_tun(const char *dns_address_first, const char *dns_address_second)
 {
 	struct Except eh;
 	char name[NAME_SIZE];
@@ -17,6 +17,9 @@ int set_dns_for_tun(const char *dns_address)
 	char reg_key_path[NAME_SIZE];
 	snprintf(reg_key_path, NAME_SIZE, "%s%s", REG_KEY_PATH_PREFIX, name);
 	
+	char dns_address[NAME_SIZE];
+	snprintf(dns_address, NAME_SIZE, "%s,%s", dns_address_first, dns_address_second);
+
 	LONG status;
 	HKEY netcard_key;
 	status = RegOpenKeyEx(HKEY_LOCAL_MACHINE, reg_key_path, 0, KEY_SET_VALUE, &netcard_key);
@@ -39,13 +42,13 @@ int set_dns_for_tun(const char *dns_address)
 int main(int argc, char* argv[])
 {
 	//set_dns_for_tun("fc5f:c567:102:c14e:326e:5035:d7e5:9f78");
-	if (argc != 2)
+	if (argc > 3)
 	{
-		printf("usage: main_dns <dns_address>\n");
+		printf("usage: main_dns <dns_address_1> <dns_address_2>\n");
 		return 1;
 	}
 	
-	int ret = set_dns_for_tun(argv[1]);
+	int ret = set_dns_for_tun(argv[1], argv[2]);
 	if (ret)
 	{
 		printf("Internal error\n");
