64d3fd90f59e080a0f58bd3b4e07dbd5bd1f4585
win <win@example.com>
2015-12-16 13:59:07 +0100

    set ipv4 dns to 8.8.8.8

--- interface/tuntap/windows/main_dns.c
+++ interface/tuntap/windows/main_dns.c
@@ -5,7 +5,8 @@
 #include <winreg.h>
 
 #define NAME_SIZE 256
-#define REG_KEY_PATH_PREFIX "SYSTEM\\ControlSet001\\services\\TCPIP6\\Parameters\\Interfaces\\"
+#define REG_KEY_PATH_PREFIX_V6 "SYSTEM\\ControlSet001\\services\\TCPIP6\\Parameters\\Interfaces\\"
+#define REG_KEY_PATH_PREFIX_V4 "SYSTEM\\ControlSet001\\services\\TCPIP\\Parameters\\Interfaces\\"
 
 int set_dns_for_tun(const char *dns_address_first, const char *dns_address_second)
 {
@@ -13,18 +14,25 @@ int set_dns_for_tun(const char *dns_address_first, const char *dns_address_secon
 	char name[NAME_SIZE];
 	char actual_name[NAME_SIZE];
 	TAPDevice_get_device_guid((char *)name, NAME_SIZE, actual_name, NAME_SIZE, &eh);
-	
-	char reg_key_path[NAME_SIZE];
-	snprintf(reg_key_path, NAME_SIZE, "%s%s", REG_KEY_PATH_PREFIX, name);
-	
+
+	char reg_key_path_v6[NAME_SIZE];
+	char reg_key_path_v4[NAME_SIZE];
+
+	snprintf(reg_key_path_v6, NAME_SIZE, "%s%s", REG_KEY_PATH_PREFIX_V6, name);
+	snprintf(reg_key_path_v4, NAME_SIZE, "%s%s", REG_KEY_PATH_PREFIX_V4, name);
+
 	char dns_address[NAME_SIZE];
 	snprintf(dns_address, NAME_SIZE, "%s,%s", dns_address_first, dns_address_second);
 
 	LONG status;
 	HKEY netcard_key;
-	status = RegOpenKeyEx(HKEY_LOCAL_MACHINE, reg_key_path, 0, KEY_SET_VALUE, &netcard_key);
+	printf("reg_key_path_v6 %s\n", reg_key_path_v6);
+	printf("reg_key_path_v4 %s\n", reg_key_path_v4);
+	// set ipv6 dns
+	printf("open\n");
+	status = RegOpenKeyEx(HKEY_LOCAL_MACHINE, reg_key_path_v6, 0, KEY_SET_VALUE, &netcard_key);
 	if (status) return status;
-
+	printf("set\n");
 	status = RegSetValueEx(netcard_key,
 	              "NameServer",
 				  0,
@@ -32,10 +40,26 @@ int set_dns_for_tun(const char *dns_address_first, const char *dns_address_secon
 				  dns_address,
 				  strlen(dns_address)+1);
 	if (status) return status;
-				  
+	printf("close\n");
 	status = RegCloseKey(netcard_key);
 	if (status) return status;
-	
+
+	printf("set ipv4 dns\n");
+	// set ipv4 dns
+	status = RegOpenKeyEx(HKEY_LOCAL_MACHINE, reg_key_path_v4, 0, KEY_SET_VALUE, &netcard_key);
+	if (status) return status;
+
+	status = RegSetValueEx(netcard_key,
+	              "NameServer",
+				  0,
+				  REG_SZ,
+				  "8.8.8.8",
+				  strlen("8.8.8.8")+1);
+	if (status) return status;
+
+	status = RegCloseKey(netcard_key);
+	if (status) return status;
+
 	return 0;
 }
 
